2b6b32eae3f074f3bf7ed76591b47827e7853688
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2015-07-16 23:27:40 +0300

    moved to angular

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function (angular) {
+  var app = angular.module('vvkp-app',[]);
+
+  app.controller('deputiesListCtrl', ['$scope', '$http', function($scope, $http){
+    $scope.list = [];
+
+    $http.get('data/data.min.js')
+	  .then(function(response){
+			var binStr = atob(response.data);
+			$scope.list = JSON.parse(pako.inflate(binStr, { to: 'string' }));
+		});
+  }]);
+
+	app.directive('vvkpSrc', function(){
+		return {
+			restrict: 'A',
+			scope: true,
+			link: function(scope, elem, attrs){
+
+				function tryToLoadImage(){
+					elem.attr('src', '')
+						.attr('src', attrs.vvkpSrc)
+						.on('error', function() {
+							tryToLoadImage();
+						});
+				}
+
+				setTimeout(tryToLoadImage,1500);
+			}
+		}
+	})
+})(window.angular)
