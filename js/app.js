ca3eb6b17c9b67eab34773da1b654d3a649604a1
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2015-07-20 10:37:58 +0300

    Improved image load

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
 (function (angular) {
-  var app = angular.module('vvkp-app',[]);
+    var app = angular.module('vvkp-app',[]);
 
-  app.controller('deputiesListCtrl', ['$scope', '$http', function($scope, $http){
-    $scope.list = [];
+    app.controller('deputiesListCtrl', ['$scope', '$http', function($scope, $http){
+        $scope.list = [];
 
-    $http.get('data/data.min.js?vvkp-version-1.2.2')
-	  .then(function(response){
-			var binStr = atob(response.data);
-			$scope.list = JSON.parse(pako.inflate(binStr, { to: 'string' }));
-		});
-  }]);
+        $http.get('data/data.min.js?vvkp-version-1.2.2')
+            .then(function(response){
+                  var binStr = atob(response.data);
+                  $scope.list = JSON.parse(pako.inflate(binStr, { to: 'string' }));
+            });
+    }]);
 
 	app.directive('vvkpSrc', function(){
 		return {
 			restrict: 'A',
 			scope: true,
-			link: function(scope, elem, attrs){
-
+			link: function(scope, elem, attrs) {
 				function tryToLoadImage(){
 					elem.attr('src', '')
 						.attr('src', attrs.vvkpSrc)
-						.on('error', function() {
+						.on('load error', function() {
 							tryToLoadImage();
 						});
 				}
-
-				setTimeout(tryToLoadImage,1500);
+                tryToLoadImage();
 			}
-		}
-	})
-})(window.angular)
+		};
+	});
+})(window.angular);
