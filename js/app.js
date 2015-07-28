d8620c189fab3dd1992e99941bb14cbae0b57dc9
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2015-07-28 12:56:18 +0300

    Fixed tags for Buchkovska

--- js/app.js
+++ js/app.js
@@ -218,7 +218,7 @@
         };
 
         // Load data
-        $http.get('data/data.min.js?vvkp-version-1.3.1')
+        $http.get('data/data.min.js?vvkp-version-1.3.2')
             .then(function(response){
                 var binStr = atob(response.data),
                     data = JSON.parse(pako.inflate(binStr, { to: 'string' }));
