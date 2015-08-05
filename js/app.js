c5cf52c470f2d619c7d69fadf8c2ec65f006659d
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2015-08-05 14:21:11 +0300

    Google Analytics

--- js/app.js
+++ js/app.js
@@ -522,29 +522,25 @@ window.onload = function() {
     }(document, 'script', 'facebook-jssdk'));
 
     // Google Analytics
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-64746354-1', 'auto', {'allowLinker': true});
-    ga('require', 'linker');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
     switch (document.domain) {
-        default:
-            ga('linker:autoLink', ['peremoga.today', 'zrada.today'] );
-            break;
         case 'peremoga.today':
-            ga('linker:autoLink', ['vvkp.in.ua', 'zrada.today'] );
+            ga('create', 'UA-64746354-4', 'auto');
             break;
         case 'zrada.today':
-            ga('linker:autoLink', ['vvkp.in.ua', 'peremoga.today'] );
+            ga('create', 'UA-64746354-5', 'auto');
+            break;
+        case 'dev.vvkp.in.ua':
+        case 'vvkp.dev':
+            ga('create', 'UA-64746354-6', 'auto');
+            break;
+        default:
+            ga('create', 'UA-64746354-1', 'auto');
             break;
     }
     ga('send', 'pageview');
+
 };
