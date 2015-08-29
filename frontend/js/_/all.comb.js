a6ddcdfbfcc93711939a1dda56d6884cf9d7f14e
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2015-08-29 11:38:27 +0300

    Styles improvement

--- frontend/js/_/all.comb.js
+++ frontend/js/_/all.comb.js
@@ -159,17 +159,6 @@ VVKP_DATA = {
     ],
     "laws": [
         {
-            "id": "відвідуваність",
-            "no": "",
-            "name": "Реєстрація депутата за допомогою електронної системи",
-            "url": "",
-            "good": 1,
-            "tagYes": "працює",
-            "descYes": "Відвідано не менше половини засідань",
-            "tagNo": "прогулює",
-            "descNo": "Прогуляно більше половини засідань"
-        },
-        {
             "id": "шокін",
             "no": "",
             "name": "Постанова про звільнення гепрокурора Віктора Шокіна",
@@ -180,6 +169,17 @@ VVKP_DATA = {
             "descYes": "Підписано постанову про звільнення гепрокурора Віктора Шокіна",
             "tagNo": "шокін-ок",
             "descNo": "Не підписано постанову про звільнення гепрокурора Віктора Шокіна"
+        },
+        {
+            "id": "відвідуваність",
+            "no": "",
+            "name": "Реєстрація депутата за допомогою електронної системи",
+            "url": "",
+            "good": 1,
+            "tagYes": "працює",
+            "descYes": "Відвідано не менше половини засідань",
+            "tagNo": "прогулює",
+            "descNo": "Прогуляно більше половини засідань"
         },
         {
             "id": "2087а",
@@ -234,19 +234,6 @@ VVKP_DATA = {
             "date": 1429563600
         },
         {
-            "id": "2172",
-            "no": "2172",
-            "name": "Законопроект про внесення змін до статті 28 Бюджетного кодексу України щодо доступу до інформації про бюджетні показники у формі відкритих даних",
-            "url": "http://w1.c1.rada.gov.ua/pls/zweb2/webproc4_1?pf3511=54101",
-            "urlVoting": "http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos?g_id=1453",
-            "good": 1,
-            "tagYes": "новатор",
-            "descYes": "Підтримано доступ до інформації про бюджетні показники у формі відкритих даних",
-            "tagNo": "ретроград",
-            "descNo": "Не підтримано доступ до інформації про бюджетні показники у формі відкритих даних",
-            "date": 1428526800
-        },
-        {
             "id": "2250",
             "no": "2250",
             "name": "Проект Закону про ринок природного газу",
@@ -257,6 +244,19 @@ VVKP_DATA = {
             "descYes": "Підтримано демонополізацію ринку природного газу",
             "tagNo": "ретроград",
             "descNo": "Не підтримано демонополізацію ринку природного газу",
+            "date": 1428526800
+        },
+        {
+            "id": "2172",
+            "no": "2172",
+            "name": "Законопроект про внесення змін до статті 28 Бюджетного кодексу України щодо доступу до інформації про бюджетні показники у формі відкритих даних",
+            "url": "http://w1.c1.rada.gov.ua/pls/zweb2/webproc4_1?pf3511=54101",
+            "urlVoting": "http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos?g_id=1453",
+            "good": 1,
+            "tagYes": "новатор",
+            "descYes": "Підтримано доступ до інформації про бюджетні показники у формі відкритих даних",
+            "tagNo": "ретроград",
+            "descNo": "Не підтримано доступ до інформації про бюджетні показники у формі відкритих даних",
             "date": 1428526800
         },
         {
@@ -273,19 +273,6 @@ VVKP_DATA = {
             "date": 1428354000
         },
         {
-            "id": "1058",
-            "no": "1058",
-            "name": "Проект Закону про внесення змін до деяких законодавчих актів України",
-            "url": "http://w1.c1.rada.gov.ua/pls/zweb2/webproc4_1?pf3511=52471",
-            "urlVoting": "http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos?g_id=805",
-            "good": 1,
-            "tagYes": "новатор",
-            "descYes": "Підтримано зняття регуляторних бар'єрів для розвитку державно-приватного партнерства в Україні та стимулювання інвестицій",
-            "tagNo": "ретроград",
-            "descNo": "Не підтримано зняття регуляторних бар'єрів для розвитку державно-приватного партнерства в Україні та стимулювання інвестицій",
-            "date": 1423688400
-        },
-        {
             "id": "1580",
             "no": "1580",
             "name": "Проект Закону про внесення змін до деяких законодавчих актів України щодо спрощення умов ведення бізнесу",
@@ -296,6 +283,19 @@ VVKP_DATA = {
             "descYes": "Підтримано дерегуляцію",
             "tagNo": "ретроград",
             "descNo": "Не підтримано дерегуляцію",
+            "date": 1423688400
+        },
+        {
+            "id": "1058",
+            "no": "1058",
+            "name": "Проект Закону про внесення змін до деяких законодавчих актів України",
+            "url": "http://w1.c1.rada.gov.ua/pls/zweb2/webproc4_1?pf3511=52471",
+            "urlVoting": "http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos?g_id=805",
+            "good": 1,
+            "tagYes": "новатор",
+            "descYes": "Підтримано зняття регуляторних бар'єрів для розвитку державно-приватного партнерства в Україні та стимулювання інвестицій",
+            "tagNo": "ретроград",
+            "descNo": "Не підтримано зняття регуляторних бар'єрів для розвитку державно-приватного партнерства в Україні та стимулювання інвестицій",
             "date": 1423688400
         }
     ],
@@ -34155,11 +34155,11 @@ VVKP_DATA = {
             return results;
         };
 
-        function searchedDeputiesLimitReset(){
+        function searchedDeputiesLimitReset() {
             $scope.searchedDeputiesLimit = 12;
         }
 
-        function searchedDeputiesLimitInc(){
+        function searchedDeputiesLimitInc() {
             if ($scope.mainTabRadio !== 'deputies') {
                 return;
             }
@@ -34173,9 +34173,9 @@ VVKP_DATA = {
                     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
                     return !check;
                 })();
-                $scope.searchedDeputiesLimit += isNotMobile ? 108 : 12;
+                $scope.searchedDeputiesLimit += isNotMobile ? 108 : 4;
                 $scope.searchedDeputiesIncreasingLimit = false;
-            }, 50);
+            }, 100);
         }
 
         function searchDeputies(deputies, parties, lawTags, searchTags) {
@@ -34358,7 +34358,7 @@ VVKP_DATA = {
 
         // Lazy loading
         window.onscroll = function() {
-            if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 200)) {
+            if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 400)) {
                 $timeout(searchedDeputiesLimitInc);
             }
         };
