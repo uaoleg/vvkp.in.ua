95a80c09ee871458d588dcb25f15cf18ed2f9260
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2015-08-29 14:55:15 +0300

    New build version

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -137,7 +137,7 @@
 
         $scope.deputyPage = function(deputy) {
             var modalInstance = $modal.open({
-                templateUrl: 'template/deputy/page.html?vvkp-version-1.8.0',
+                templateUrl: 'template/deputy/page.html?vvkp-version-2.0.0',
                 scope: $scope,
                 animation: true
             });
