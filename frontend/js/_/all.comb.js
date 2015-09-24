87f32a114b66ad014bac82451eaebc25a341a3e0
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2015-09-24 10:02:58 +0300

    New build version

--- frontend/js/_/all.comb.js
+++ frontend/js/_/all.comb.js
@@ -34112,7 +34112,7 @@ VVKP_DATA = {
 
         $scope.deputyPage = function(deputy) {
             var modalInstance = $modal.open({
-                templateUrl: 'template/deputy/page.html?vvkp-version-2.0.2',
+                templateUrl: 'template/deputy/page.html?vvkp-version-2.0.3',
                 scope: $scope,
                 animation: true
             });
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -137,7 +137,7 @@
 
         $scope.deputyPage = function(deputy) {
             var modalInstance = $modal.open({
-                templateUrl: 'template/deputy/page.html?vvkp-version-2.0.2',
+                templateUrl: 'template/deputy/page.html?vvkp-version-2.0.3',
                 scope: $scope,
                 animation: true
             });
