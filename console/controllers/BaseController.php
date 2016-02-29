8c88fab61b1471dc10bc8ac6c3bd399a1f61b864
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2016-02-29 17:03:40 +0200

    Improved CLI on windows

--- console/controllers/BaseController.php
+++ console/controllers/BaseController.php
@@ -4,4 +4,28 @@ namespace console\controllers;
 class BaseController extends \yii\console\Controller
 {
 
+    /**
+     * Init
+     */
+    public function init()
+    {
+        parent::init();
+
+        // Support cyrillic on windows
+        if ($this->isWindows()) {
+            system("chcp 65001");
+        }
+    }
+
+    /**
+     * Check if env OS is Windows
+     * @return bool
+     */
+    protected function isWindows()
+    {
+        $os = getenv('OS');
+        $isWindows = (stripos($os, 'windows') !== false);
+        return $isWindows;
+    }
+
 }
